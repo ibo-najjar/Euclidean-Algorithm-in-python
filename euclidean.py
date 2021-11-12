6c29fd64d3ad3295846440b80bd4dcffd53e5eb3
ibo <83659352+ibo-najjar@users.noreply.github.com>
2021-11-12 17:19:50 +0300

    Update euclidean.py

--- euclidean.py
+++ euclidean.py
@@ -1,3 +1,6 @@
+import time
+
+
 def main():
     factors = []
     num1, num2 = input("ENTER 2 NUMBERS TO FIND GCD (FORMAT => NUM1<SPACE>NUM2)\n").split()
@@ -7,17 +10,23 @@ def main():
     if num2 > big:
         big = num2
         small = num1
-    gcd = euclidean(big, small, factors)
-    print(f'greatest common factor of {big} and {small} is \n{gcd}')
+    start_time = time.perf_counter()
+    gcd = eudi(big, small, factors)
+    end_time = time.perf_counter()
+    total_time = "{:.20f}".format(end_time - start_time)
+
+    print(f'greatest common factor of {big} and {small} is \n{gcd}\nexecution time {total_time}')
 
 
-def euclidean(a, b, factors):
+def eudi(a, b, factors):
+    a = int(a)
+    b = int(b)
     q = int(a / b)
     r = a - b * q
     if r != 0:
         factors.append(r)
         print(b, r)
-        return euclidean(b, r, factors)
+        return eudi(b, r, factors)
     else:
         return factors[-1]
 
