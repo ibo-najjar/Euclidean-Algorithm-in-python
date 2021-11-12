310084af48757feb538289abdcdd44753792267f
ibo <83659352+ibo-najjar@users.noreply.github.com>
2021-11-12 16:33:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    factors = []
+    num1, num2 = input("ENTER 2 NUMBERS TO FIND GCD (FORMAT => NUM1<SPACE>NUM2)\n").split()
+    num2 = int(num2)
+    big = int(num1)
+    small = int(num2)
+    if num2 > big:
+        big = num2
+        small = num1
+    gcd = eudi(big, small, factors)
+    print(f'greatest common factor of {big} and {small} is \n{gcd}')
+
+
+def eudi(a, b, factors):
+    q = int(a / b)
+    r = a - b * q
+    if r != 0:
+        factors.append(r)
+        print(b, r)
+        return eudi(b, r, factors)
+    else:
+        return factors[-1]
+
+
+if __name__ == "__main__":
+    main()
